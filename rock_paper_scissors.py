c7631e56b6990c643a551653d3bcc19cb2c220ce
Javohir <68422551+Jonikulov@users.noreply.github.com>
2021-03-20 14:26:49 +0500

    added main code

---
+++ rock_paper_scissors.py
@@ -0,0 +1,99 @@
+# ROCK, PAPER, SCISSORS
+import random
+
+elements = ['ROCK', 'PAPER', 'SCISSORS']
+print('ROCK, PAPER, SCISSORS')
+win = loss = tie = 0
+
+while True:
+    result = f"{win} Wins, {loss} Losses, {tie} Ties \n" 
+    move = f"Enter your move: (r)ock, (p)aper, (s)cissors or (q)uit"
+    print(result, move)
+    answer = input(">>> ")
+    # if you choose 'ROCK'
+    if answer == 'r':
+        pc = random.choice(elements)
+        print('ROCK versus', pc)
+        # selection probabilities of the PC, when you chose ‘rock’
+        if pc == 'ROCK':
+            print("It is a tie!")
+            tie += 1
+        elif pc == 'PAPER':
+            print('You lose!')
+            loss += 1
+        else:
+            print('You win!')
+            win += 1
+    # if you choose 'PAPER'
+    elif answer == 'p':
+        pc = random.choice(elements)
+        print('PAPER versus', pc)
+        # selection probabilities of the PC, when you chose ‘paper’
+        if pc == 'PAPER':
+            print("It is a tie!")
+            tie += 1
+        elif pc == 'SCISSORS':
+            print('You lose!')
+            loss += 1
+        else:
+            print('You win!')
+            win += 1
+    # if you choose 'SCISSORS'
+    elif answer == 's':
+        pc = random.choice(elements)
+        print('SCISSORS versus', pc)
+        # selection probabilities of the PC, when you chose ‘scissors’
+        if pc == 'SCISSORS':
+            print("It is a tie!")
+            tie += 1
+        elif pc == 'ROCK':
+            print('You lose!')
+            loss += 1
+        else:
+            print('You win!')
+            win += 1
+    # when the program's finished, the final results are displayed
+    elif answer == 'q':
+        print(result)
+        break
+    else:
+        print('Please enter only the characters mentioned.')
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
